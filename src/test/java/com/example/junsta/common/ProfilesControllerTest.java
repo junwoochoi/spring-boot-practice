02ec5cf8ccb9b245d59e582b39ed24ecfbaf3db3
junwoochoi <cjw4690@gmail.com>
2019-03-07 13:20:26 +0900

    add ProfilesController

---
+++ src/test/java/com/example/junsta/common/ProfilesControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.junsta.common;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class ProfilesControllerTest extends BaseControllerTest {
+
+    @Test
+    public void  getProfile() throws Exception {
+        ResultActions perform = mockMvc.perform(get("/profile"));
+
+        String response = perform.andReturn().getResponse().getContentAsString();
+
+        assertThat(response).isEqualTo("test");
+    }
+}
