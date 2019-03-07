4b22ee54460734029d2b45f32ab25d981a82d2c1
junwoochoi <cjw4690@gmail.com>
2019-03-07 14:54:07 +0900

    to check operation server error

--- src/main/java/com/example/junsta/SpringBootJunstaApplication.java
+++ src/main/java/com/example/junsta/SpringBootJunstaApplication.java
@@ -1,24 +1,25 @@
 package com.example.junsta;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@Slf4j
 public class SpringBootJunstaApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "/app/config/spring-boot-practice-sns/real-application.yml";
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/spring-boot-practice-sns/real-application.yml";
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(SpringBootJunstaApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
-	}
+    public static void main(String[] args) {
+        log.error(APPLICATION_LOCATIONS);
+        new SpringApplicationBuilder(SpringBootJunstaApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 
 }
 
